fa6f21b163e333e83253a23ff5320b11809462bc
Klojo <goldenkooy@gmail.com>
2019-09-27 00:06:30 +0200

    Create resize.py

---
+++ sample/resize.py
@@ -0,0 +1,16 @@
+def resize(folder):
+    '''
+        # Example resize('/root/Bureaublad/data/boefjes/front')
+    '''
+    from PIL import Image
+    import os,sys,glob
+    for root, dirs, files in os.walk(folder, topdown=False):
+        print('[Resizing '+str(len(files))+' files]')
+        for name in files:
+            print(os.path.join(root, name))
+            im = Image.open(os.path.join(root, name))
+            rgb_im = im.convert('RGB')
+            name, ext = name.split('.'):
+            outfilename = '/'+name+'%d.'+ext % int(i + 1)
+            outfile=os.path.join(directory, outfilename)
+            print(directory+outfile)
