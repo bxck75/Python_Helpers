c6b59c336d5147888c8a81ebb5647e550625e0e5
Klojo <goldenkooy@gmail.com>
2019-09-26 21:45:10 +0200

    Update main.py

--- main.py
+++ main.py
@@ -7,6 +7,6 @@ sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..')
 import sample
 
 class main():
-    def __init__():
+    def __init__(self):
         Helper=sample.BigHelp.Helpers
         Zipper=sample.ZipUp
