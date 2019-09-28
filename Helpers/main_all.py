ffc0de6a8657fbd4a9423990b900164c0369df75
Klojo <goldenkooy@gmail.com>
2019-09-28 20:00:37 +0200

    Update and rename main_began.py to main_all.py

--- Helpers/main_all.py
+++ Helpers/main_all.py
@@ -1,9 +1,13 @@
 import tensorflow as tf
 import os
 from .BEGAN import *
+from .DCGAN import *
+
+
 
 flags = tf.app.flags
 flags.DEFINE_bool("is_training", False, "training flag")
+flags.DEFINE_bool("gan", 'BEGAN', "training flag")
 FLAGS = flags.FLAGS
 
 def check_dir():
@@ -20,10 +24,15 @@ def main():
     config = tf.ConfigProto(gpu_options=gpu_options)
     config.gpu_options.allow_growth=True
     with tf.Session(config=config) as sess:
-        began = BEGAN(input_height=64, input_width=64, input_channels=3, output_height=64, output_width=64, gf_dim=64, input_fname_pattern = '*.jpg', is_grayscale=False, sess = sess)
-        began.build_model()
+        if FLAGS.gan:
+            gan = FLAGS.gan(input_height=64, input_width=64, input_channels=3, output_height=64, output_width=64, gf_dim=64, input_fname_pattern = '*.jpg', is_grayscale=False, sess = sess)
+            gan.build_model()
+        
         if FLAGS.is_training:
-            began.train()
+            gan.train()
+            
+#         gan = BEGAN(input_height=64, input_width=64, input_channels=3, output_height=64, output_width=64, gf_dim=64, input_fname_pattern = '*.jpg', is_grayscale=False, sess = sess)
+#         gan.build_model()
     
 if __name__=='__main__':
     main()
