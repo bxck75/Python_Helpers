7cabc0660aac539fed2855a347503480a3db22dd
Klojo <goldenkooy@gmail.com>
2019-10-08 07:59:35 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,12 +1,56 @@
-# -*- coding: utf-8 -*-
-
-import sys
-import os
-sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
-
+import os, sys, inspect
 import Helpers
 
-class main():
+class main:
+    
     def __init__(self):
-        Helper=sample.BigHelp.Helpers
-        Zipper=sample.ZipUp
+        ''' import helpers '''
+        os.chdir(helpers_root)
+        
+        ''' start helper core and system commands '''
+        Helpers_Core=Helpers.Core() # new helper class
+        hlp=Helpers_Core.H  # old  module helpers
+        HelpMe = Helpers_Core.H.Me # build in system commands old  module helpers
+        Sys_Exec = Helpers_Core.Sys_Exec
+
+        ''' scraper install '''
+        c_d(root)
+        dosage = ['bxck75/dosage']
+        HelpMe(['inst_reps', dosage,  helpers_root+'/Helpers', False, True])
+
+        ''' needed gdrive repos '''
+        gd_rps=[
+            'bxck75/google-drive-list-shared',
+            'bxck75/PyDrive',
+        ]
+        HelpMe(['inst_reps',gd_rps, helpers_root+'/Helpers',False,True])
+
+        ''' PyDrive install '''
+        Sys_Exec('python /content/installed_repos/Python_Helpers/Helpers/PyDrive/setup.py install')
+        import pydrive
+
+        ''' google shared wrapper '''
+        Sys_Exec('cp ' + helpers_root + '/Helpers/google-drive-list-shared/google-drive-list-shared.py ' + helpers_root + '/Helpers/gdrive_shared.py')
+        Sys_Exec('rm -r ' + helpers_root + '/Helpers/google-drive-list-shared')
+
+        ''' pix2pix repos '''
+        pix2pix_rps=[
+            'bxck75/piss-ant-pix2pix',
+            'bxck75/scrape-linkedin-selenium',
+            # 'bxck75/opencv',
+            ]
+        HelpMe(['inst_reps',pix2pix_rps, root +'/installed_repos',False,True])
+
+        ''' many repos in this list!!! '''
+        Helpers.RepCoList.repos_sorted
+
+        ''' zipper init '''
+        Zipper = Helpers_Core.load_zipper()
+
+        ''' gdrive downloader init '''
+        Gdrive_download = Helpers_Core.Gdownload.GdriveD
+
+        ''' image crawler init'''
+        ImgCrawler = Helpers.core.GoImgScrape.GoogleImageCrawler()
+        
+M = main()
