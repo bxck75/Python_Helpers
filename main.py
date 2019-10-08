ecddbaca6c9161bc75373114321ea48603f1179e
Klojo <goldenkooy@gmail.com>
2019-10-08 08:14:30 +0200

    Update main.py

--- main.py
+++ main.py
@@ -6,61 +6,61 @@ class main:
     def __init__(self):
         
         ''' set root paths '''
-        root = '/content'
-        gdrive_root = '/content/drive/My Drive'
-        helpers_root = root + '/installed_repos/Python_Helpers'
+        self.root = '/content'
+        self.gdrive_root = '/content/drive/My Drive'
+        self.helpers_root = self.root + '/installed_repos/Python_Helpers'
 
         ''' import helpers '''
-        os.chdir(helpers_root)
+        os.chdir(self.helpers_root)
         
         ''' start helper core and system commands '''
-        Helpers_Core=Helpers.Core() # new helper class
-        hlp=Helpers_Core.H  # old  module helpers
-        HelpMe = Helpers_Core.H.Me # build in system commands old  module helpers
-        Sys_Exec = Helpers_Core.Sys_Exec
+        self.Helpers_Core=Helpers.Core() # new helper class
+        self.hlp=self.Helpers_Core.H  # old  module helpers
+        self.HelpMe = self.Helpers_Core.H.Me # build in system commands old  module helpers
+        self.Sys_Exec = self.Helpers_Core.Sys_Exec
 
         ''' Better chdir '''
-        c_d = Helpers_Core.cd
-        c_d(root)
+        self.c_d = self.Helpers_Core.cd
+        self.c_d(self.root)
         
         ''' scraper install '''
-        c_d(root)
-        dosage = ['bxck75/dosage']
-        HelpMe(['inst_reps', dosage,  helpers_root+'/Helpers', False, True])
+        self.c_d(self.root)
+        self.dosage = ['bxck75/dosage']
+        self.HelpMe(['inst_reps', self.dosage,  self.helpers_root+'/Helpers', False, True])
 
         ''' needed gdrive repos '''
-        gd_rps=[
+        self.gd_rps=[
             'bxck75/google-drive-list-shared',
             'bxck75/PyDrive',
         ]
-        HelpMe(['inst_reps',gd_rps, helpers_root+'/Helpers',False,True])
+        self.HelpMe(['inst_reps',self.gd_rps, self.helpers_root+'/Helpers',False,True])
 
         ''' PyDrive install '''
-        Sys_Exec('python /content/installed_repos/Python_Helpers/Helpers/PyDrive/setup.py install')
+        self.Sys_Exec('python /content/installed_repos/Python_Helpers/Helpers/PyDrive/setup.py install')
         import pydrive
 
         ''' google shared wrapper '''
-        Sys_Exec('cp ' + helpers_root + '/Helpers/google-drive-list-shared/google-drive-list-shared.py ' + helpers_root + '/Helpers/gdrive_shared.py')
-        Sys_Exec('rm -r ' + helpers_root + '/Helpers/google-drive-list-shared')
+        self.Sys_Exec('cp ' + self.helpers_root + '/Helpers/google-drive-list-shared/google-drive-list-shared.py ' + self.helpers_root + '/Helpers/gdrive_shared.py')
+        self.Sys_Exec('rm -r ' + self.helpers_root + '/Helpers/google-drive-list-shared')
 
         ''' pix2pix repos '''
-        pix2pix_rps=[
+        self.pix2pix_rps=[
             'bxck75/piss-ant-pix2pix',
             'bxck75/scrape-linkedin-selenium',
             # 'bxck75/opencv',
             ]
-        HelpMe(['inst_reps',pix2pix_rps, root +'/installed_repos',False,True])
+        self.HelpMe(['inst_reps',self.pix2pix_rps, self.root +'/installed_repos',False,True])
 
         ''' many repos in this list!!! '''
-        Helpers.RepCoList.repos_sorted
+        self.Helpers.RepCoList.repos_sorted
 
         ''' zipper init '''
-        Zipper = Helpers_Core.load_zipper()
+        self.Zipper = self.Helpers_Core.load_zipper()
 
         ''' gdrive downloader init '''
-        Gdrive_download = Helpers_Core.Gdownload.GdriveD
+        self.Gdrive_download = self.Helpers_Core.Gdownload.GdriveD
 
         ''' image crawler init'''
-        ImgCrawler = Helpers.core.GoImgScrape.GoogleImageCrawler()
+        self.ImgCrawler = self.Helpers.core.GoImgScrape.GoogleImageCrawler()
         
 M = main()
