be551e64a038809a749aaf1b7318b16edc72b38b
Klojo <goldenkooy@gmail.com>
2019-10-08 07:05:00 +0200

    Update core.py

--- Helpers/core.py
+++ Helpers/core.py
@@ -138,11 +138,11 @@ class Core:
 
         if self.if_exists(self.system_log_file):
             fh = open(self.system_log_file, 'a+' )
-            fh.write(str(msg)+"\n")
+            fh.write(str(msg)+'\n')
             fh.close()
         else:
             fh = open(self.system_log_file, 'w' )
-            fh.write(str(log_name ,'logfile')+"\n")
+            fh.write(str(log_name +'logfile')+'\n')
             fh.close()
                    
     def runProcess(self):
