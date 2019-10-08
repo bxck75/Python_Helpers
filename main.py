e7c3edb1460dabd261d4485f79570a2cb0e95713
Klojo <goldenkooy@gmail.com>
2019-10-08 22:42:52 +0200

    Update main.py

--- main.py
+++ main.py
@@ -14,8 +14,8 @@ class main:
         
         ''' start helper core and system commands '''
         self.Helpers_Core=Helpers.Core() # new helper class
-        self.hlp=Helpers.BigHelp.H  # old  module helpers
-        self.HelpMe = Helpers.BigHelp.H.Me # build in system commands old  module helpers
+#         self.hlp=Helpers.BigHelp.Helpers.H  # old  module helpers
+        self.HelpMe = Helpers.BigHelp.Helpers.H.Me # build in system commands old  module helpers
         self.Sys_Exec = self.Helpers_Core.Sys_Exec
 
         ''' Better chdir '''
