bc9ff1d34b67978446f198a6c6f1fd56e01fe5bb
Klojo <goldenkooy@gmail.com>
2019-10-08 09:48:55 +0200

    Update ICrawl.py

--- Helpers/ICrawl.py
+++ Helpers/ICrawl.py
@@ -1,24 +1,25 @@
 from icrawler.builtin import BaiduImageCrawler, BingImageCrawler, GoogleImageCrawler
 
 class ICrawl():
-    def __init__(self, keyword, max_qty=10, out_dir='icrawl_loot_images', min_wh=(200,200), max_wh=(800,800)):
+    def __init__(self, keyword='abc', max_qty=10, out_dir='icrawl_loot_images', min_wh=(200,200), max_wh=(800,800)):
+        
         self.out_dir = out_dir
         self.keyword = keyword
         self.max_qty = max_qty
         self.min_wh = min_wh
         self.max_wh = max_wh
         
-        self.google_crawler = GoogleImageCrawler(feeder_threads=1,
-                                                    parser_threads=1,
-                                                    downloader_threads=4,
-                                                    storage={'/content' : out_dir})
+        self.google_crawler = GoogleImageCrawler(feeder_threads=1,parser_threads=1,downloader_threads=4, storage={'/content' : out_dir})
         
         self.filters = dict(size='large', color='orange', license='commercial,modify', date=((2019, 1, 1), (2019, 6, 30)))
-        
         
         self.google_crawler.crawl(keyword=self.keyword, filters=self.filters, offset=0, max_num=self.max_qty,
                                         min_size=self.min_wh, max_size=self.max_wh, file_idx_offset=0)
 
+        
+        
+        
+        
 #         bing_crawler = BingImageCrawler(downloader_threads=4,
 #                                         storage={'root_dir': 'your_image_dir'})
 #         bing_crawler.crawl(keyword='cat', filters=None, offset=0, max_num=1000)
