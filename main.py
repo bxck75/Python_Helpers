8971fea6c827cc5f6812715ac0711c32e2ba31e5
Klojo <goldenkooy@gmail.com>
2019-10-09 09:56:44 +0200

    Update main.py

--- main.py
+++ main.py
@@ -25,38 +25,38 @@ class main:
         ''' scraper install '''
         self.c_d(self.root)
         
-        ''' In_helpers/helpers/ map ''' 
-        inst_dir=self.helpers_root+'/Helpers'
-        repos=['bxck75/piss_ant_pix2pix','bxck75/A1Colabs']
-        self.Helpers_Core.install_repos(repos, inst_dir,False,True)
+#         ''' In_helpers/helpers/ map ''' 
+#         inst_dir=self.helpers_root+'/Helpers'
+#         repos=['bxck75/piss_ant_pix2pix','bxck75/A1Colabs']
+#         self.Helpers_Core.install_repos(repos, inst_dir,False,True)
 
-        ''' cv2 and distro install '''
-        cv_repos=[
-            'bxck75/opencv_contrib',
-            'bxck75/opencv',
-            'bxck75/face2face-demo',
-            'bxck75/face-recognition',
-        ]
-        self.Helpers_Core.install_repos(cv_repos, inst_dir,False,True) 
+#         ''' cv2 and distro install '''
+#         cv_repos=[
+#             'bxck75/opencv_contrib',
+#             'bxck75/opencv',
+#             'bxck75/face2face-demo',
+#             'bxck75/face-recognition',
+#         ]
+#         self.Helpers_Core.install_repos(cv_repos, inst_dir,False,True) 
         
-        ''' needed gdrive repos '''
-        gdrive_rps=[
-            'bxck75/google-drive-list-shared', 
-            'bxck75/PyDrive'
-        ]
-        self.Helpers_Core.install_repos(gdrive_rps, inst_dir,False,True)
+#         ''' needed gdrive repos '''
+#         gdrive_rps=[
+#             'bxck75/google-drive-list-shared', 
+#             'bxck75/PyDrive'
+#         ]
+#         self.Helpers_Core.install_repos(gdrive_rps, inst_dir,False,True)
 
-        ''' PyDrive install '''
-        self.Sys_Exec('python /content/installed_repos/Python_Helpers/Helpers/PyDrive/setup.py install')
-        import pydrive
-        self.pydr= pydrive
+#         ''' PyDrive install '''
+#         self.Sys_Exec('python /content/installed_repos/Python_Helpers/Helpers/PyDrive/setup.py install')
+#         import pydrive
+#         self.pydr= pydrive
         
-        ''' google shared wrapper '''
-        self.Sys_Exec('cp ' + self.helpers_root + '/Helpers/google-drive-list-shared/google-drive-list-shared.py ' + self.helpers_root + '/Helpers/gdrive_shared.py')
-        self.Sys_Exec('rm -r ' + self.helpers_root + '/Helpers/google-drive-list-shared')
+#         ''' google shared wrapper '''
+#         self.Sys_Exec('cp ' + self.helpers_root + '/Helpers/google-drive-list-shared/google-drive-list-shared.py ' + self.helpers_root + '/Helpers/gdrive_shared.py')
+#         self.Sys_Exec('rm -r ' + self.helpers_root + '/Helpers/google-drive-list-shared')
 
-        ''' pix2pix repos '''
-        pix2pix_rps=['bxck75/piss-ant-pix2pix','bxck75/dosage']
-        self.Helpers_Core.install_repos(pix2pix_rps, inst_dir,False,True)
+#         ''' pix2pix repos '''
+#         pix2pix_rps=['bxck75/piss-ant-pix2pix','bxck75/dosage']
+#         self.Helpers_Core.install_repos(pix2pix_rps, inst_dir,False,True)
         
 # main()
