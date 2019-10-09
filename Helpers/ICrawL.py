3594834c36d5c1b63530f27af279301af6d132ea
Klojo <goldenkooy@gmail.com>
2019-10-09 12:22:28 +0200

    Create ICrawL.py

---
+++ Helpers/ICrawL.py
@@ -0,0 +1,46 @@
+from icrawler.builtin import BaiduImageCrawler, BingImageCrawler, GoogleImageCrawler
+
+def ICL(key='face', out_dir='/content/images'):
+    '''IcrawL(key, out_dir='/content/images')'''
+    
+    google_crawler = GoogleImageCrawler(
+        feeder_threads=1,
+        parser_threads=1,
+        downloader_threads=4,
+        storage={'root_dir': out_dir })
+ 
+    filters = dict(
+        size='medium',
+        # color='orange',
+        # license='commercial,modify',
+        # date=((2017, 1, 1), (2017, 11, 30)),
+        )    
+    google_crawler.crawl(
+        keyword=key, 
+        filters=filters, 
+        offset=0, 
+        max_num=10,
+        min_size=(400,400), 
+        max_size=None, 
+        file_idx_offset=0,
+        )
+    bing_crawler = BingImageCrawler(
+        downloader_threads=4,
+        storage={'root_dir': out_dir }
+        )
+    bing_crawler.crawl(
+        keyword=key, 
+        filters=None, 
+        offset=0, 
+        max_num=1000
+        )
+    baidu_crawler = BaiduImageCrawler(
+        storage={'root_dir': out_dir }
+        )
+    baidu_crawler.crawl(
+        keyword=key, 
+        offset=0, 
+        max_num=1000,
+        min_size=(200,200), 
+        max_size=None
+        )
