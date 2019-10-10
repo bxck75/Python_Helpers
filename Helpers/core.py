bcd062b053b7291191ee2ab02db3e68ddae9f9a1
Klojo <goldenkooy@gmail.com>
2019-10-11 01:28:32 +0200

    Update core.py

--- Helpers/core.py
+++ Helpers/core.py
@@ -206,14 +206,14 @@ class Core:
         ''' Download and unzip the haar cascader '''
         predictor_file = ['shape_predictor_194_face_landmarks.zip','1fMOT_0f5clPbZXsphZyrGcLXkIhSDl3o']
         self.GdriveD.GdriveD(predictor_file[1],predictor_file[0])
-        os.system('unzip /content/shape_predictor_194_face_landmarks.zip')
+        os.system('unzip ' + os.path.join(self.git_install_root, 'shape_predictor_194_face_landmarks.zip') + ' -d ' + self.git_install_root)
         
         ''' Detector predictor load'''
         import dlib
-        predictor = predictor_file[0].replace('zip','dat')
+        predictor = self. + predictor_file[0].replace('zip','dat')
         detector = dlib.get_frontal_face_detector()
         predictor = dlib.shape_predictor(predictor)
-        self.FaceAligner = self.DFace.AlignDlib(predictor_file[0])
+        self.FaceAligner = self.DFace.AlignDlib(os.path.join(self.git_install_root , predictor_file[0]))
         plt.imshow(self.FaceAligner.getAllFaceBoundingBoxes(img_in))
         plt.show
     
