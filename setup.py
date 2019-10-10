cdda3f008f84e114ef8e1053b71b991006c39430
Klojo <goldenkooy@gmail.com>
2019-10-10 21:11:02 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -10,13 +10,44 @@ with open('LICENSE') as f: license = f.read()
 
 setup(
     name='Helpers',
-    version='0.1.3',
+    version='0.1.4',
     description='Helpers package for Me, myself and I',
-    long_description=readme,
-    author='Boudewijn Kooij',
+    author='Boudewijn Kooy',
     author_email='goldenkooy@gmail.com',
+    maintainer='K00B404',
+    maintainer_email='koob404@gmail.com',
+    long_description=readme,
     url='https://github.com/bxck75/Python_Helpers',
     license=license,
-    packages=find_packages(exclude=('tests', 'docs'))
+    packages=find_packages(exclude=('tests', 'docs'))'
+    install_requires=[
+        "google-api-python-client >= 1.2",
+        "oauth2client >= 4.0.0",
+        "PyYAML >= 3.0",
+        "pydrive >= 1.3",
+        "colorama",
+    ],
 )
+# Example bellow
+
+# from setuptools import setup
+
+# setup(
+#     name='PyDrive',
+#     version='1.3.1',
+#     author='JunYoung Gwak',
+#     author_email='jgwak@dreamylab.com',
+#     maintainer='Robin Nabel',
+#     maintainer_email='rnabel@ucdavis.edu',
+#     packages=['pydrive', 'pydrive.test'],
+#     url='https://github.com/gsuitedevs/PyDrive',
+#     license='LICENSE',
+#     description='Google Drive API made easy.',
+#     long_description='Read the README.rst',
+#     install_requires=[
+#         "google-api-python-client >= 1.2",
+#         "oauth2client >= 4.0.0",
+#         "PyYAML >= 3.0",
+#     ],
+# )
 
