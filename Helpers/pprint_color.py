28661547d07240f1cbb356eb2ec2d46cd4b7e515
Klojo <goldenkooy@gmail.com>
2019-10-11 00:10:11 +0200

    Update pprint_color.py

--- Helpers/pprint_color.py
+++ Helpers/pprint_color.py
@@ -4,4 +4,4 @@ from pygments.formatters import Terminal256Formatter
 from pprint import pformat
 
 def pprint_color(obj):
-    print highlight(pformat(obj), PythonLexer(), Terminal256Formatter())
+    print(highlight(pformat(obj), PythonLexer(), Terminal256Formatter()))
