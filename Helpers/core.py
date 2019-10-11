4b621c5fa3e541dfe1163f504ce0ab47eccde99d
Klojo <goldenkooy@gmail.com>
2019-10-11 19:26:14 +0200

    Update core.py

--- Helpers/core.py
+++ Helpers/core.py
@@ -590,7 +590,7 @@ class Core:
 		  gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
 		  faces = face_cascade.detectMultiScale(gray, 1.3, 5)
 		  for (x,y,w,h) in faces:
-			sampleNum = sampleNum+1
+			sampleNum = sampleNum+ 1
 			cv2.imwrite("dataset/User."+str(uid)+"."+str(sampleNum)+".jpg",gray[y:y+h,x:x+w])
 			cv2.rectangle(img, (x,y), (x+w, y+h), (255,0,0), 2)
 			cv2.waitKey(100)
