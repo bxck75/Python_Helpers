eb7811ff4b27c18f6282d1114fdcfc6fbb21d6df
Klojo <goldenkooy@gmail.com>
2019-10-16 03:05:00 +0200

    Update Python_Helper_local_jumpstart.py

--- Python_Helper_local_jumpstart.py
+++ Python_Helper_local_jumpstart.py
@@ -1,7 +1,7 @@
 import os, sys, inspect
 from pathlib import Path
-root = sys.argv[1]
-# root = '/content'
+# root = sys.argv[1]
+root = '/content'
 # os.chdir(root)
 helpers_path = root + '/installed_repos/Python_Helpers'
 os.makedirs(helpers_path , exist_ok=True)
@@ -9,4 +9,4 @@ os.system('sudo rm -r ' + helpers_path) # clear for new pull from git
 os.system('git clone https://github.com/bxck75/Python_Helpers.git '+helpers_path) # git clone the helpers
 os.chdir(helpers_path)
 os.system('python setup.py install')
-os.system('ls ' + helpers_path)
+os.system('python cli_main.py')
